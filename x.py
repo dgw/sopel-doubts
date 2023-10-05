6d1f4d7ae20a2301eca3da6d27a3e8ac06590bd0
xnaas <me@xnaas.info>
2023-10-05 16:18:19 -0500

    ' => "

--- x.py
+++ x.py
@@ -1,11 +1,10 @@
 """
 Authors: xnaas <me@xnaas.info> (2022)
 License: The Unlicense (public domain)
-Require: python>=3.8
 """
 from sopel import plugin
 
 
-@plugin.rule(r'^[XⓍ]$')
+@plugin.rule(r"^[XⓍ]$")
 def x_to_doubt(bot, trigger):
-	bot.action('doubts')
+	bot.action("doubts")
