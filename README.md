ba79a385a095a6e07337b3c98c0e6674a2eab7d9
xnaas <me@xnaas.info>
2023-11-17 20:14:48 -0600

    packaging!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sopel-doubts
 Press `X` to doubt.
+
+## Install
+```sh
+pip install --index-url https://git.actionsack.com/api/packages/xnaas/pypi/simple/ -U sopel-doubts
+```
+
+## X
+Triggers on these characters (case-insensitive):
+* X
+* Ⓧ
+* 𝕏
--- sopel_doubts/plugin.py
+++ sopel_doubts/plugin.py
@@ -1,9 +1,5 @@
-"""
-Authors: xnaas <me@xnaas.info> (2022-2023)
-License: The Unlicense (public domain)
-"""
+# source: https://git.actionsack.com/xnaas/sopel-doubts
 from sopel import plugin
-
 
 @plugin.rule(r"^[XⓍ𝕏]$")
 def x_to_doubt(bot, trigger):
