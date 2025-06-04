070eb52843630edbd111e1f35c59aa6f991a38bc
dgw <dgw@technobabbl.es>
2025-06-04 16:57:44 -0500

    Fix lowercase 𝕩 not being recognized
    As the comment says, the double struck X characters are caseless. This
    seems to be on purpose as part of Unicode.

--- sopel_doubts/plugin.py
+++ sopel_doubts/plugin.py
@@ -5,6 +5,9 @@ https://git.actionsack.com/xnaas/sopel-doubts
 """
 from sopel import plugin
 
-@plugin.rule(r"^[XⓍ𝕏]$")
+
+# yes, both cases of the double struck X need to be included; they are caseless,
+# so a case-insensitive regex can't match both with just one.
+@plugin.rule(r"^[XⓍ𝕏𝕩]$")
 def x_to_doubt(bot, trigger):
 	bot.action("doubts")
